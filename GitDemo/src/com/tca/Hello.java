f67986b1cc5105c6f33a3d520a104a7ba77f2037
akshayspatil9309-design <akshayspatil9309@gmail.com>
2026-08-11 22:15:01 +0530

    Addition

--- GitDemo/src/com/tca/Hello.java
+++ GitDemo/src/com/tca/Hello.java
@@ -2,6 +2,13 @@ package com.tca;
 
 public class Hello {
     public static void main(String[] args) {
+    	
     	System.out.println("Hello GitHub - I am learning Git...!");
+    	
+    	int a=10,b=20,c;
+    	c=a+b;
+    	
+    	System.out.println("Addition "+c);
+    	
     	
     }}

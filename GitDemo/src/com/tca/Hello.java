1ae37d252277fb15553467b4b638c5a00122b246
akshayspatil9309-design <akshayspatil9309@gmail.com>
2026-08-11 21:50:28 +0530

    changes done

--- GitDemo/src/com/tca/Hello.java
+++ GitDemo/src/com/tca/Hello.java
@@ -2,6 +2,6 @@ package com.tca;
 
 public class Hello {
     public static void main(String[] args) {
-    	System.out.println("Hello GitHub - I am learning Git!");
+    	System.out.println("Hello GitHub - I am learning Git! Completed First Push");
     	
     }}

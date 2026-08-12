610fe443f10d14496993c6b1c457ce778093a5be
akshayspatil9309-design <akshayspatil9309@gmail.com>
2026-08-12 14:56:30 +0530

    Updated Hello message by Developer B

--- GitDemo/src/com/tca/Hello.java
+++ GitDemo/src/com/tca/Hello.java
@@ -3,7 +3,7 @@ package com.tca;
 public class Hello {
     public static void main(String[] args) {
     	
-    	System.out.println("Hello GitHub - I am learning Git...!");
+    	System.out.println("Hello from Developer B");
     	
     	int a=10,b=20,c;
     	c=a+b;
